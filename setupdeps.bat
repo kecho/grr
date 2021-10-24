4a0282781fb97cbe81e3db5d62a0f7aad7d8706f
Kleber Garcia <kleber.garcia@unity3d.com>
2021-10-24 15:00:54 -0400

    More deps on quaternion

--- setupdeps.bat
+++ setupdeps.bat
@@ -1,4 +1,6 @@
 pip install --upgrade pip
-pip install numpy==1.21.2
+pip install numpy==1.20
+pip install numba==0.54.1
+pip install scipy==1.7.1
 pip install numpy-quaternion==2021.10.7.23.40.37
 pip install PyWavefront==1.3.3
