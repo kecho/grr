adbe6a539234abf80dc6d8aa12092d632dceffd4
Kleber Garcia <kleber.garcia@unity3d.com>
2021-11-06 21:43:21 -0400

    Cleaning code a bit

--- grr/geometry.hlsl
+++ grr/geometry.hlsl
@@ -2,31 +2,6 @@
 
 namespace geometry
 {
-
-    struct Vertex
-    {
-        float3 p;
-        float3 n;
-        float2 uv;
-    };
-
-    struct Triangle
-    {
-        int a;
-        int b;
-        int c;
-    };
-
-    Triangle sampleTriangle(Buffer<int> indices, int triangleId)
-    {
-        Triangle t;
-        int i = 3 * triangleId;
-        t.a = indices[i + 0];
-        t.b = indices[i + 1];
-        t.c = indices[i + 2];
-        return t;
-    }
-
     float triangleArea(float2 a, float2 b, float2 c)
     {
         return 0.5 * (a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y));
@@ -40,7 +15,6 @@ namespace geometry
         float2 bari = float2(area1 / totalArea, area2 / totalArea);
         return float3(bari.x, bari.y, 1.0 - (bari.x + bari.y));
     }
-
     float3 computeBaryCoordPerspective(float3 aw, float3 bw, float3 cw, float2 p)
     {
         float3 b = computeBaryCoord(aw.xy, bw.xy, cw.xy, p);
@@ -48,6 +22,99 @@ namespace geometry
         B /= (B.x + B.y + B.z);
         return B;
     }
+
+    struct Vertex
+    {
+        float3 p;
+        float3 n;
+        float2 uv;
+    };
+
+    struct TriangleI
+    {
+        int a;
+        int b;
+        int c;
+    
+        void load(Buffer<int> indices, int triangleId)
+        {
+            int i = 3 * triangleId;
+            a = indices[i + 0];
+            b = indices[i + 1];
+            c = indices[i + 2];
+        }
+    };
+
+    struct TriangleV
+    {
+        Vertex a;
+        Vertex b;
+        Vertex c;
+
+        void load(StructuredBuffer<Vertex> vertices, in TriangleI indices)
+        {
+            a = vertices[indices.a];
+            b = vertices[indices.b];
+            c = vertices[indices.c];
+        }
+    };
+
+    struct TriInterpResult
+    {
+        bool visible;
+        float3 bari;
+
+        float3 eval(float3 a, float3 b, float3 c)
+        {
+            return a * bari.x + b * bari.y + c * bari.z;
+        }
+    };
+
+    struct TriangleH
+    {
+        float4 h0;
+        float4 h1;
+        float4 h2;
+
+        float3 p0;
+        float3 p1;
+        float3 p2;
+
+        void init(TriangleV tri, float4x4 view, float4x4 proj)
+        {
+            h0 = mul(mul(float4(tri.a.p.xyz, 1.0), view), proj);
+            h1 = mul(mul(float4(tri.b.p.xyz, 1.0), view), proj);
+            h2 = mul(mul(float4(tri.c.p.xyz, 1.0), view), proj);
+
+            p0 = h0.xyz / h0.w;
+            p1 = h1.xyz / h1.w;
+            p2 = h2.xyz / h2.w;
+        }
+
+        TriInterpResult interp(float2 hCoords)
+        {
+            float2 ea = p1.xy - p0.xy;
+            float2 eb = p2.xy - p1.xy;
+            float2 ec = p0.xy - p2.xy;
+
+            float2 pa = hCoords - p0.xy;
+            float2 pb = hCoords - p1.xy;
+            float2 pc = hCoords - p2.xy;
+
+            float wa = ea.x * pa.y - ea.y * pa.x;
+            float wb = eb.x * pb.y - eb.y * pb.x;
+            float wc = ec.x * pc.y - ec.y * pc.x;
+
+            float frontFace = -max(wa, max(wb, wc));
+            float backFace = min(wa, min(wb, wc));
+
+            TriInterpResult result;
+            result.bari = computeBaryCoordPerspective(float3(p0.xy,h0.w), float3(p1.xy,h1.w), float3(p2.xy,h2.w), hCoords);
+            result.visible = (frontFace > 0.0) || (backFace > 0.0);
+            return result;
+        }
+    };
+
 }
 
 #endif
--- grr/raster.hlsl
+++ grr/raster.hlsl
@@ -22,41 +22,21 @@ void csMainRaster(int3 dispatchThreadId : SV_DispatchThreadID)
     float2 sc = float2(sin(rot), cos(rot));
     float2x2 rotm = float2x2(sc.x, sc.y, -sc.y, sc.x);
 
-    geometry::Triangle t = geometry::sampleTriangle(g_indices, 0);
-    geometry::Vertex a = g_verts[t.a];
-    geometry::Vertex b = g_verts[t.b];
-    geometry::Vertex c = g_verts[t.c];
+    geometry::TriangleI ti;
+    ti.load(g_indices, 0);
 
-    a.p.xz = mul(a.p.xz, rotm);
-    b.p.xz = mul(b.p.xz, rotm);
-    c.p.xz = mul(c.p.xz, rotm);
+    geometry::TriangleV tv;
+    tv.load(g_verts, ti);
 
-    float4 ta = mul(mul(float4(a.p.xyz, 1.0), g_view), g_proj);
-    float4 tb = mul(mul(float4(b.p.xyz, 1.0), g_view), g_proj);
-    float4 tc = mul(mul(float4(c.p.xyz, 1.0), g_view), g_proj);
-    a.p = (ta / ta.w).xyz;
-    b.p = (tb / tb.w).xyz;
-    c.p = (tc / tc.w).xyz;
+    tv.a.p.xz = mul(tv.a.p.xz, rotm);
+    tv.b.p.xz = mul(tv.b.p.xz, rotm);
+    tv.c.p.xz = mul(tv.c.p.xz, rotm);
 
-    float2 ea = b.p.xy - a.p.xy;
-    float2 eb = c.p.xy - b.p.xy;
-    float2 ec = a.p.xy - c.p.xy;
+    geometry::TriangleH th;
+    th.init(tv, g_view, g_proj);
 
-    float2 pa = hCoords - a.p.xy;
-    float2 pb = hCoords - b.p.xy;
-    float2 pc = hCoords - c.p.xy;
-
-    float wa = ea.x * pa.y - ea.y * pa.x;
-    float wb = eb.x * pb.y - eb.y * pb.x;
-    float wc = ec.x * pc.y - ec.y * pc.x;
-
-    float3 barys = geometry::computeBaryCoordPerspective(float3(a.p.xy,ta.w), float3(b.p.xy,tb.w), float3(c.p.xy,tc.w), hCoords);
-    float3 debugCol1 = float3(1,0,0) * barys.x;
-    float3 debugCol2 = float3(0,1,0) * barys.y;
-    float3 debugCol3 = float3(0,0,1) * barys.z;
-    float3 finalCol = barys.x > 0.5 ? float3(1,0,0) : (barys.y > 0.5 ? float3(0,1,0) : (barys.z > 0.5 ? float3(0,0,1) : (debugCol1 + debugCol2 + debugCol3)));
-
-    float frontFace = -max(wa, max(wb, wc));
-    float backFace = min(wa, min(wb, wc));
-    g_output[dispatchThreadId.xy] = (frontFace > 0.0) || (backFace > 0.0) ? float4(finalCol,0) : float4(0,0,0,1);
+    geometry::TriInterpResult interpResult = th.interp(hCoords);
+    
+    float3 finalCol = interpResult.eval(float3(1,0,0), float3(0,1,0), float3(0,0,1));
+    g_output[dispatchThreadId.xy] = interpResult.visible ? float4(finalCol, 1) : float4(0,0,0,1);
 }
