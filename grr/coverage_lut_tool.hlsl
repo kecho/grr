a6320426a8db05f6584fcc08df0ff0b777c351f9
Kleber Garcia <kecho_garcia@hotmail.com>
2022-07-25 00:37:57 -0400

    ore work on coverage lut

--- grr/coverage_lut_tool.hlsl
+++ grr/coverage_lut_tool.hlsl
@@ -1,15 +1,109 @@
+#include "debug_font.hlsl"
 
 cbuffer Constants : register(b0)
 {
     float4 g_size; //w,h,1/w,1/h
+    float4 g_packedTri0;
+    float4 g_packedTri1;
 }
 
+struct InputTri
+{
+    float2 v0;
+    float2 v1;
+    float2 v2;
+
+    void load()
+    {
+        float2 aspect = float2(g_size.x * g_size.w, 1.0);
+        v0 = g_packedTri0.xy * aspect;    
+        v1 = g_packedTri0.zw * aspect;    
+        v2 = g_packedTri1.xy * aspect;    
+    }
+};
+
+SamplerState g_fontSampler : register(s0);
+Texture2D<float4> g_fontTexture : register(t0);
 RWTexture2D<float4> g_output : register(u0);
+
+float2 getGridUV(float2 uv)
+{
+    return uv * 8.0;
+}
+
+float4 drawGrid(float2 uv, uint2 bitMask)
+{
+    float2 gridUV = getGridUV(uv);
+    int2 gridCoord = (int2)gridUV;
+    int gridIndex = gridCoord.y * 8 + gridCoord.x;
+    
+    gridUV = frac(gridUV);
+
+    float4 numCol = Font::drawNumber(
+        g_fontTexture, g_fontSampler, gridUV * float2(2.0,4.0), 2, gridIndex);
+
+    float4 col = ((gridCoord.x + (gridCoord.y & 0x1)) & 0x1) ? float4(1,1,1,0.4) : float4(0.5,0.5,0.5,0.4);
+
+    col.rgb += numCol.rgb * numCol.a;
+    int bitVal =  ((gridIndex >= 32) ? (bitMask.x >> (gridIndex - 32)) : (bitMask.y >> gridIndex)) & 0x1; 
+    if (bitVal)
+    {
+        col = lerp(col, float4(0.6, 0, 0, 1), length(gridUV * 2.0 - 1.0) < 0.4 ? 1.0 : 0.0);
+    }
+
+    return col;
+}
+
+float3 drawVertex(float3 col, float2 v, float2 uv)
+{
+    float d = distance(v, uv);
+    if (d < 0.01)
+        return float3(0.8,0.8,0.0);
+    return col;
+}
+
+float3 drawLine(float3 col, float2 v0, float2 v1, float2 uv)
+{
+    float ldist = distance(v1, v0);
+    float2 lv = v1 - v0;
+    float2 ld = lv/ldist;
+    float2 ruv = uv - v0;
+    float t = dot(ld, ruv);
+    if (t < 0.0 || t > ldist)
+        return col;
+
+    float2 hitPoint = t * ld + v0;
+    if (distance(hitPoint, uv) < 0.005)
+        return float3(0.0, 0.0, 1.0);
+    return col;
+}
 
 [numthreads(8,8,1)]
 void csMain(
     uint2 dispatchThreadID : SV_DispatchThreadID)
 {
-    //g_output[dispatchThreadID.xy] = float4(dispatchThreadID.xy,0.0,1.0) * float4(g_size.zw, 1.0, 1.0);
-    g_output[dispatchThreadID.xy] = float4(dispatchThreadID.x * g_size.z > 0.5 ? float3(1,0,0) : float3(0,0,1), 1.0);
+    uint2 pixelCoord = dispatchThreadID.xy;
+    float aspect = g_size.x * g_size.w;
+    float2 screenUv = float2(pixelCoord) * g_size.zw * float2(aspect, 1.0);
+    float2 boardOffset = 0.5 * aspect * float2((g_size.x - g_size.y), 0.0) * g_size.zw;
+    float2 boardUv = screenUv - boardOffset;
+    float3 color = float3(0,0,0);
+
+    InputTri tri;
+    tri.load();
+    color = drawLine(color, tri.v0, tri.v1, screenUv);
+    color = drawLine(color, tri.v1, tri.v2, screenUv);
+    color = drawLine(color, tri.v2, tri.v0, screenUv);
+    color = drawVertex(color, tri.v0, screenUv);
+    color = drawVertex(color, tri.v1, screenUv);
+    color = drawVertex(color, tri.v2, screenUv);
+
+    if (all(boardUv >= 0.0) && all(boardUv <= 1.0))
+    {
+        uint2 mask = uint2(0, 1251512);
+        float4 gridCol = drawGrid(boardUv, mask);
+        color = lerp(color, gridCol.rgb, saturate(gridCol.a));
+    }
+
+    g_output[pixelCoord] = float4(color, 1.0);
 }
--- grr/coverage_lut_tool.py
+++ grr/coverage_lut_tool.py
@@ -1,4 +1,5 @@
 import coalpy.gpu as g
+from . import debug_font
 import math
 
 g_coverage_lut_tool_shader = g.Shader(file="coverage_lut_tool.hlsl", name="coverage_lut_tool", main_function = "csMain")
@@ -9,6 +10,12 @@ class CoverageLUTTool:
         self.m_texture = None
         self.m_tex_width = 0
         self.m_tex_height = 0
+        self.m_v0x = 0.2
+        self.m_v0y = 0.2
+        self.m_v1x = 0.5
+        self.m_v1y = 0.9
+        self.m_v2x = 0.9
+        self.m_v2y = 0.2
         return
 
     @property
@@ -28,7 +35,11 @@ class CoverageLUTTool:
         cmd.dispatch(
             shader = g_coverage_lut_tool_shader,
             constants = [
-                float(self.m_tex_width), float(self.m_tex_height), 1.0/float(self.m_tex_width), 1.0/float(self.m_tex_height)],
+                float(self.m_tex_width), float(self.m_tex_height), 1.0/float(self.m_tex_width), 1.0/float(self.m_tex_height),
+                float(self.m_v0x), float(self.m_v0y), float(self.m_v1x), float(self.m_v1y),
+                float(self.m_v2x), float(self.m_v2y), 0.0, 0.0],
+            samplers = [debug_font.font_sampler],
+            inputs = [debug_font.font_texture],
             outputs = self.m_texture,
 
             x = math.ceil(self.m_tex_width/8),
@@ -41,7 +52,14 @@ class CoverageLUTTool:
 
     def build_ui(self, imgui : g.ImguiBuilder):
         self.m_active = imgui.begin("Coverage LUT Tool", self.m_active)
-        (cr_min_w, cr_min_h) = imgui.get_window_content_region_min()
+        self.m_v0x = imgui.slider_float(label="v0x", v=self.m_v0x, v_min=0.0, v_max=1.0)
+        self.m_v0y = imgui.slider_float(label="v0y", v=self.m_v0y, v_min=0.0, v_max=1.0)
+        self.m_v1x = imgui.slider_float(label="v1x", v=self.m_v1x, v_min=0.0, v_max=1.0)
+        self.m_v1y = imgui.slider_float(label="v1y", v=self.m_v1y, v_min=0.0, v_max=1.0)
+        self.m_v2x = imgui.slider_float(label="v2x", v=self.m_v2x, v_min=0.0, v_max=1.0)
+        self.m_v2y = imgui.slider_float(label="v2y", v=self.m_v2y, v_min=0.0, v_max=1.0)
+
+        (cr_min_w, cr_min_h) = imgui.get_cursor_pos()
         (cr_max_w, cr_max_h) = imgui.get_window_content_region_max()
         (nw, nh) = (int(cr_max_w - cr_min_w), int(cr_max_h - cr_min_h))
 
@@ -54,4 +72,5 @@ class CoverageLUTTool:
 
         if self.m_texture != None:
             imgui.image(texture = self.m_texture, size = (self.m_tex_width, self.m_tex_height))
+
         imgui.end()
